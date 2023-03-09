86a5feace5c36276fcff91f80cb0e4890e52e7d3
Natsirt87 <tristancommons@outlook.com>
2023-03-08 21:13:00 -0800

    Added loading button spinner thing

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { ref, onMounted } from "vue";
 const queryData = ref();
+const loadingLoad = ref(false);
 
 async function load() {
   // Call the API method for load
+  loadingLoad.value = true;
 
   const response = await fetch("/api/load", { method: "PUT" });
   console.log(await response.json());
+
+  loadingLoad.value = false;
 }
 
 function clear() {
@@ -31,10 +35,21 @@ function query(firstName, lastName) {
     <div class="flex justify-center gap-4 sm:gap-10 mt-10">
       <button 
         type="button"
-        class="squishy-button bg-blue-500 focus:ring-4 focus:ring-blue-500/50 shadow-lg shadow-slate-800 px-14 sm:px-20 py-3"
+        class="squishy-button bg-blue-500 focus:ring-4 focus:ring-blue-500/50 shadow-lg shadow-slate-800 w-28 sm:w-44 py-3"
+        :class="{ 'opacity-60': loadingLoad}"
+        :disabled="loadingLoad"
         @click="load"
       >
-        Load
+        <svg v-if="loadingLoad" class="h-6 w-6 animate-spin" viewBox="3 3 18 18">
+          <path
+            class="fill-blue-800"
+            d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"></path>
+          <path
+            class="fill-blue-100"
+            d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"></path>
+        </svg>
+
+        <span v-else>Load</span>
       </button>
 
       <button
@@ -90,6 +105,8 @@ function query(firstName, lastName) {
   line-height: 1.25;
 }
 .squishy-button {
+  display: flex;
+  justify-content: center;
   border-radius: 7px;
   color: white;
   outline: none;
